10c452f5dfca30f0ffdfcc8ed1d80581be11c41e
ElAsJay <tjdwnwkd12371@gmail.com>
2022-09-25 18:10:05 +0900

    [BOJ] 숫자 / 브론즈2
    https://www.acmpic.net/problem/10093

--- ElAsJay/10093.숫자.py
+++ ElAsJay/10093.숫자.py
@@ -1,5 +1,10 @@
 a,b = map(int, input().split())
+d = abs(a-b)
 
-print(b-a-1)
-for x in range(a+1,b):
+if d<=1:
+    print(0)
+else:
+    print(d-1)
+
+for x in range(min(a,b)+1, max(a,b)):
     print(x, end=" ")
