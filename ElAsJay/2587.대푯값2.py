574bf5fe6dd353ab724329967c6278401fcd04ce
ElAsJay <tjdwnwkd12371@gmail.com>
2022-09-25 18:10:57 +0900

    [BOJ] 대푯값2 / 브론즈2
    https://www.acmpic.net/problem/2587

--- ElAsJay/2587.대푯값2.py
+++ ElAsJay/2587.대푯값2.py
@@ -1,9 +1,14 @@
 nums = []
 sum = 0
+
+# 5개 숫자 입력, 입력한 숫자의 합 계산
 for _ in range(5):
     a = int(input())
     nums.append(a)
     sum += a
+
+# 입력한 숫자 배열 정렬
 nums.sort()
+
 print(round(sum/5))
 print(nums[2])
