8cce85fb327415e1401cb49180b333d88f5f5507
mayo_ <59410565+hyerang0125@users.noreply.github.com>
2022-09-25 19:18:29 +0900

    [BOJ] 10869 사칙연산 | 브론즈 5
    - 문제 주소: https://www.acmicpc.net/problem/10869

---
+++ hyerang0125/0x02/boj_10869.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    cout << a+b << endl;
+    cout << a-b << endl;
+    cout << a*b << endl;
+    cout << a/b << endl;
+    cout << a%b << endl;
+
+    return 0;
+}
