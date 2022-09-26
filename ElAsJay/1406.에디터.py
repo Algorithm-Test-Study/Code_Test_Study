9fa2954e71003f63552c760a2e09ea7d2db78e00
ElAsJay <tjdwnwkd12371@gmail.com>
2022-09-26 15:16:45 +0900

    [BOJ] 에디터 / 실버2
    https://www.acmpic.net/problem/1406

--- ElAsJay/1406.에디터.py
+++ ElAsJay/1406.에디터.py
@@ -1,27 +1,32 @@
 import sys
 
-stk1 = list(sys.stdin.readline().split())
+stk1 = list(sys.stdin.readline().strip())
 stk2 = []
 
 for _ in range(int(input())):
     mes = list(sys.stdin.readline().split())
     op = mes[0]
 
-    if op == "P":
+    if op == 'P':
         stk1.append(mes[1])
 
-    elif op == "L":
+    elif op == 'L':
         if stk1:
             stk2.append(stk1.pop())
+        else:
+            continue
 
-    elif op == "D":
+    elif op == 'D':
         if stk2:
             stk1.append(stk2.pop())
+        else:
+            continue
 
-    elif op == "B":
+    elif op == 'B':
         if stk1:
             stk1.pop()
-
+        else:
+            continue
     else:
         break
     
