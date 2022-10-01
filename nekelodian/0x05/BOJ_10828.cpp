87bbf3d98c29af7338afbee90d16e0814340e266
nekelodian <89613687+nekelodian@users.noreply.github.com>
2022-10-02 03:43:21 +0900

    [BOJ] 스택 / 실버 4

--- nekelodian/0x05/BOJ_10828.cpp
+++ nekelodian/0x05/BOJ_10828.cpp
@@ -36,3 +36,59 @@ int main(void){
     }
     return 0;
 }
+
+/*
+//스택구현
+#include <bits/stdc++.h>
+using namespace std;
+const int MX = 100000;
+int dat[MX];
+int pos = 0;
+
+void push(int x){
+    dat[pos++] = x;
+}
+void pop(){
+    pos--;
+}
+int top(){
+    return dat[pos-1];
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    stack<int> S;
+    int n;
+    cin >> n;
+    while(n--){
+        string c;
+        cin >> c;
+        if(c=="push"){
+            int i;
+            cin >> i;
+            push(i);
+        }else if(c=="pop"){
+            if(pos == 0)
+                cout << "-1\n";
+            else{
+                cout << top() << '\n';
+                pop();
+            }
+        }else if(c=="size"){
+            cout << pos << '\n';
+        }else if(c=="empty"){
+            if(pos == 0)
+                cout << "1\n";
+            else
+                cout << "0\n";
+        }else if(c=="top"){
+            if(pos == 0)
+                cout << "-1\n";
+            else
+                cout << top() << '\n';
+        }
+    }
+    return 0;
+}
+*/
