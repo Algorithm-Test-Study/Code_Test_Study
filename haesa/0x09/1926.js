805318ed5d67e4d4b8ee14045ce23240b6337db3
haesa <xdhaesa@gmail.com>
2022-10-07 01:45:33 +0900

    [BOJ] 그림 / 실버1 => 코드수정
    https://www.acmicpc.net/problem/1926

--- haesa/0x09/1926.js
+++ haesa/0x09/1926.js
@@ -1,42 +1,51 @@
-const input = require('fs')
+const [nm, ...input] = require('fs')
   .readFileSync('/dev/stdin')
   .toString()
   .trim()
   .split('\n');
 
-const [n, m] = input[0].split(' ').map(Number);
-const picture = new Array(n);
-for (let i = 0; i < n; i++)
-  picture[i] = input[i + 1].trim().split(' ').map(Number);
+const [n, m] = nm.split(' ').map(Number);
+const picture = input.map((s) => s.split(' ').map(Number));
 
-const visit = new Array(n);
-for (let i = 0; i < n; i++) visit[i] = Array.from({ length: m }, () => 0);
+function isRange(x, y) {
+  return x >= 0 && x < n && y >= 0 && y < m;
+}
 
-const queue = [];
 const dx = [0, 1, 0, -1];
 const dy = [1, 0, -1, 0];
-let num = 0;
-let max = 0,
-  area = 0;
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < m; j++) {
-    if (visit[i][j] || picture[i][j] === 0) continue;
-    queue.push([i, j]);
-    visit[i][j] = 1;
-    num++;
-    while (queue.length) {
-      let cur = queue.shift();
-      area++;
-      for (let k = 0; k < 4; k++) {
-        const nx = cur[0] + dx[k];
-        const ny = cur[1] + dy[k];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-        if (visit[nx][ny] || picture[nx][ny] !== 1) continue;
-        queue.push([nx, ny]);
-        visit[nx][ny] = 1;
-      }
+
+function bfs(i, j) {
+  let area = 0;
+  picture[i][j] = 0;
+  const queue = [[i, j]];
+  while (queue.length) {
+    let [x, y] = queue.shift();
+    area++;
+    for (let k = 0; k < 4; k++) {
+      const nx = x + dx[k];
+      const ny = y + dy[k];
+      if (!isRange(nx, ny) || picture[nx][ny] === 0) continue;
+      picture[nx][ny] = 0;
+      queue.push([nx, ny]);
     }
-    area > max && (max = area);
-    area = 0;
   }
+  return area;
 }
+
+let num = 0;
+let max = 0;
+let area = 0;
+
+function solution() {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (picture[i][j] === 0) continue;
+      num++;
+      area = bfs(i, j);
+      area > max && (max = area);
+    }
+  }
+  console.log(num);
+  console.log(max);
+}
+solution();
