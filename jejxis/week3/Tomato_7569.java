622b6b05c3920640423d82fa8b83a208000c0fe7
jejxis <69408166+jejxis@users.noreply.github.com>
2022-10-10 02:45:04 +0900

    [BOJ] 토마토 / 골드5

---
+++ jejxis/week3/Tomato_7569.java
@@ -0,0 +1,85 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+// [BOJ] 토마토 / 골드5 _완
+public class Tomato_7569 {
+    public void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m = Integer.parseInt(st.nextToken());//z
+        int n = Integer.parseInt(st.nextToken());//y
+        int h = Integer.parseInt(st.nextToken());//x
+        //x: 높이, y: 세로,z: 가로
+
+        int[][][] dist= new int[h][n][m];
+        int[][][] box= new int[h][n][m];
+
+        int[] dx = {-1, 0, 0, 0, 0, 1};
+        int[] dy = {0, -1, 0, 1, 0, 0};
+        int[] dz = {0, 0, -1, 0, 1, 0};
+
+        Queue<XYZ> q = new LinkedList<>();
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < n; j++){
+                st = new StringTokenizer(br.readLine());
+                for(int k = 0; k < m; k++){
+                    box[i][j][k] = Integer.parseInt(st.nextToken());
+                    if(box[i][j][k] == 1){
+                        q.add(new XYZ(i,j,k));
+                    }
+                    if(box[i][j][k] == 0){
+                        dist[i][j][k] = -1;
+                    }
+                }
+            }
+        }
+        while(!q.isEmpty()){
+            XYZ cur = q.poll();
+            for(int d = 0; d < 6; d++){
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
+                int nz = cur.z + dz[d];
+                if(nx < 0 || nx >= h || ny < 0 || ny >= n || nz < 0 || nz >= m) continue;
+                if(dist[nx][ny][nz] >= 0 ) continue;
+                dist[nx][ny][nz] = dist[cur.x][cur.y][cur.z] +1;
+                q.add(new XYZ(nx, ny, nz));
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k < m; k++){
+                    if(dist[i][j][k] == -1){
+                        System.out.println(-1);
+                        return;
+                    }
+                    if(dist[i][j][k] > ans) ans = dist[i][j][k];
+                }
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        new Tomato_7569().solution();
+    }
+    class XYZ {
+        int x;
+        int y;
+        int z;
+
+        public XYZ(int x, int y, int z) {
+            super();
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
