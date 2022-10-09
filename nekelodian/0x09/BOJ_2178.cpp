71b7c37d542eea03029dc1fa7644b3238ebf4389
nekelodian <89613687+nekelodian@users.noreply.github.com>
2022-10-09 16:09:29 +0900

    [BOJ] 미로 탐색 / 실버 1

---
+++ nekelodian/0x09/BOJ_2178.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+string board[100];
+int dist[100][100] = {0};
+bool vis[100][100];
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    queue<pair<int,int>> q;
+    int n, m;
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        cin >> board[i];
+    }
+    for(int i=0; i<n; i++)  fill(dist[i], dist[i]+m, 0);
+    q.push({0,0});
+    dist[0][0] = 1;
+    while(!q.empty()){
+        pair<int,int> cur = q.front(); q.pop();
+        for(int dir=0; dir<4; dir++){
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m)  continue;
+            if(dist[nx][ny] > 0 || board[nx][ny] == '0')   continue;
+            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+            q.push({nx,ny});
+        }
+    }
+    cout << dist[n-1][m-1];
+    return 0;
+}
