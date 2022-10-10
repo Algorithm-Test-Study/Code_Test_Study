0b6577953e00a0d6c2c9687f3ce25b681724cbff
jejxis <69408166+jejxis@users.noreply.github.com>
2022-10-10 14:02:05 +0900

    [BOJ] 유기농 배추 / 실버2

--- jejxis/week3/OrganicBaechu_1012.java
+++ jejxis/week3/OrganicBaechu_1012.java
@@ -5,13 +5,13 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
+//[BOJ] 유기농 배추 / 실버2
 public class OrganicBaechu_1012 {
     public void solution() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
-
+        //영역 개수 세기 -> 그림 문제와 비슷
 
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, -1, 0, 1};
@@ -23,9 +23,9 @@ public class OrganicBaechu_1012 {
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
 
-            int[][] field = new int[n][m];
-            int[][] visit = new int[n][m];
-            int count = 0;
+            int[][] field = new int[n][m];//배추밭
+            int[][] visit = new int[n][m];//방문여부
+            int count = 0;//영역 개수
             for(int j = 0; j < k; j++){
                 st = new StringTokenizer(br.readLine());
                 int y = Integer.parseInt(st.nextToken());
@@ -34,18 +34,18 @@ public class OrganicBaechu_1012 {
             }
             for(int i = 0; i < n; i++){
                 for(int j = 0; j < m; j++){
-                    if(field[i][j] == 0 || visit[i][j] == 1) continue;
-                    count++;
+                    if(field[i][j] == 0 || visit[i][j] == 1) continue;//배추가 없거나 이미 방문
+                    count++;//영역 개수 증가
                     Queue<Pair> q = new LinkedList<>();
-                    visit[i][j] = 1;
+                    visit[i][j] = 1;//현재 위치 방문표시
                     q.add(new Pair(i, j));
                     while(!q.isEmpty()){
                         Pair cur = q.poll();
                         for(int d = 0; d < 4; d++){
                             int nx = cur.x + dx[d];
                             int ny = cur.y + dy[d];
-                            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                            if(visit[nx][ny] == 1 || field[nx][ny] != 1) continue;
+                            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;//범위밖
+                            if(visit[nx][ny] == 1 || field[nx][ny] != 1) continue;//방문했거나 배추가 없거나
                             visit[nx][ny] = 1;
                             q.add(new Pair(nx, ny));
                         }
