7d6b1417a7afbe9da58d1a0889b7f7f98bdf0d0c
jejxis <69408166+jejxis@users.noreply.github.com>
2022-10-10 14:11:58 +0900

    [BOJ] 적록색약 / 골드5

--- jejxis/week3/RedGreenColorWeakness_10026.java
+++ jejxis/week3/RedGreenColorWeakness_10026.java
@@ -12,7 +12,7 @@ public class RedGreenColorWeakness_10026 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int weak = 0; int not = 0;//색약인 사람이 보는 영역 수/ 아닌 사림이 보는 영역 ㅅ수
+        int weak = 0; int not = 0;//색약인 사람이 보는 영역 수/ 아닌 사람이 보는 영역 수
 
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
@@ -20,8 +20,8 @@ public class RedGreenColorWeakness_10026 {
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, -1, 0, 1};
 
-        char[][] picture = new char[n][n];
-        int[][] visit = new int[n][n];
+        char[][] picture = new char[n][n];//그림 색 배치
+        int[][] visit = new int[n][n];//방문여부
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             String s = st.nextToken();
@@ -30,12 +30,12 @@ public class RedGreenColorWeakness_10026 {
             }
         }
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){//적록색약이 아닌 사람
             for(int j = 0; j < n; j++){
                 if(visit[i][j] == 1) continue;
-                not++;
+                not++;//영역 수 증가
                 Queue<Pair> q = new LinkedList<>();
-                char curc = picture[i][j];
+                char curc = picture[i][j];//현재 색 저장
                 visit[i][j] = 1;
                 q.add(new Pair(i,j));
                 while(!q.isEmpty()){
@@ -43,36 +43,36 @@ public class RedGreenColorWeakness_10026 {
                     for(int k = 0; k < 4; k++){
                         int nx = cur.x + dx[k];
                         int ny = cur.y + dy[k];
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                        if(curc != picture[nx][ny] || visit[nx][ny] == 1) continue;//br, bg
+                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;//범위 밖
+                        if(curc != picture[nx][ny] || visit[nx][ny] == 1) continue;//같은 색이 아니거나 이미 방문
                         visit[nx][ny] = 1;
                         q.add(new Pair(nx, ny));
                     }
                 }
             }
         }
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){//적록색약 사전 작업
             for(int j = 0; j < n; j++){
-                visit[i][j] = 0;
-                if(picture[i][j] == 'R') picture[i][j] = 'G';
+                visit[i][j] = 0;//방문 표시 0으로 저장
+                if(picture[i][j] == 'R') picture[i][j] = 'G';//R은 G로 바꾸기
             }
         }
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){//적록색약인 사람
             for(int j = 0; j < n; j++){
-                if(visit[i][j] == 1) continue;
-                weak++;
+                if(visit[i][j] == 1) continue;//이미 방문
+                weak++;//영역 수 증가
                 Queue<Pair> q = new LinkedList<>();
-                char curc = picture[i][j];
-                visit[i][j] = 1;
+                char curc = picture[i][j];//현재 색
+                visit[i][j] = 1;//방문 표시
                 q.add(new Pair(i,j));
                 while(!q.isEmpty()){
                     Pair cur = q.poll();
                     for(int k = 0; k < 4; k++){
                         int nx = cur.x + dx[k];
                         int ny = cur.y + dy[k];
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                        if(curc != picture[nx][ny] || visit[nx][ny] == 1) continue;//br, bg
+                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;//범위 밖
+                        if(curc != picture[nx][ny] || visit[nx][ny] == 1) continue;//다른 색 이거나 이미 방문
                         visit[nx][ny] = 1;
                         q.add(new Pair(nx, ny));
                     }
