0db45e92ed7d79f64301c55423f7e460fd82336f
jejxis <69408166+jejxis@users.noreply.github.com>
2022-10-10 14:13:34 +0900

    [BOJ] 나이트의 이동 / 실버1

--- jejxis/week3/KnightMovement_7562.java
+++ jejxis/week3/KnightMovement_7562.java
@@ -12,49 +12,49 @@ public class KnightMovement_7562 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
 
-        int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};
-        int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
+        int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};//확인할 x 방향(세로) 순서
+        int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};//확인할 y 방향(가로) 순서
         for(int i = 0; i < T; i++){
             st = new StringTokenizer(br.readLine());
             int l = Integer.parseInt(st.nextToken());
             int[][] dis =  new int[l][l];
 
-            for(int k = 0; k < l; k++){
+            for(int k = 0; k < l; k++){//횟수 초기화
                 for(int j = 0; j < l; j++){
                     dis[k][j] = -1;
                 }
             }
 
             st = new StringTokenizer(br.readLine());
-            int curx = Integer.parseInt(st.nextToken());
+            int curx = Integer.parseInt(st.nextToken());//현재 위치
             int cury = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
-            int goalx = Integer.parseInt(st.nextToken());
+            int goalx = Integer.parseInt(st.nextToken());//목표 위치
             int goaly = Integer.parseInt(st.nextToken());
 
-            if(curx == goalx && cury == goaly) {
+            if(curx == goalx && cury == goaly) {//이미 목표 위치라면 끝내기
                 System.out.println(0);
                 continue;
             }
             Queue<Pair> q = new LinkedList<>();
-            dis[curx][cury] = 0;
+            dis[curx][cury] = 0;//현재 위치에 횟수 저장
             q.add(new Pair(curx, cury));
-            while(!q.isEmpty() && dis[goalx][goaly] == -1){
+            while(!q.isEmpty() && dis[goalx][goaly] == -1){//이동할 곳이 남아있고 도착하지 않았을 때
                 Pair cur = q.poll();
                 for(int d = 0; d < 8; d++){
                     int nx = cur.x + dx[d];
                     int ny = cur.y + dy[d];
-                    if(nx < 0 || nx >= l || ny < 0 || ny >= l) continue;
-                    if(dis[nx][ny] >= 0) continue;
+                    if(nx < 0 || nx >= l || ny < 0 || ny >= l) continue;//범위 밖
+                    if(dis[nx][ny] >= 0) continue;//이미 방문
                     dis[nx][ny] = dis[cur.x][cur.y]+1;
-                    if(nx == goalx && ny == goaly){
+                    if(nx == goalx && ny == goaly){//도착
                         break;
                     }
                     q.add(new Pair(nx, ny));
                 }
             }
-            System.out.println(dis[goalx][goaly]);
+            System.out.println(dis[goalx][goaly]);//결과 출력
         }
     }
 
